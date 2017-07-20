6b5f648e81aa8889c8791301cecdc6b92ae6d2bc
Gabe Bravo <gabecbravo@gmail.com>
2017-07-20 15:15:39 -0400

    added routing between bookshelf and search pages.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import './index.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// components
+import BookShelf from './App'
+import Search from './Search'
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={BookShelf} />
+      <Route path="/search" component={Search} />
+      <Route />
+    </Switch>
+  </Router>
+, document.getElementById('root'))
